2f8b3ebbf7bbb67e75cca0108794fdc1600520b3
PabloSan1997 <jos_pab_997@hotmali.com>
2023-02-28 17:49:40 -0600

    Configuracion inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Searched } from "./components/Searched"
+
+function App() {
+  return (
+    <div className="App">
+      <Searched/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Searched.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Input } from "antd";
+
+
+function Searched() {
+  return (
+    <Input.Search placeholder="Buscar..."/>
+  )
+}
+
+export  {Searched}
